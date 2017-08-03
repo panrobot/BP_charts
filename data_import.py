d482c1a2f9ff9e2aad6844bd7fccc7b9e3a6f9df
panrobot <panrobot@users.noreply.github.com>
2017-08-03 23:22:02 +0200

    Corectly creates BP table but one series is missing

--- data_import.py
+++ data_import.py
@@ -21,8 +21,12 @@ if __name__ == '__main__':
         
     colsOnPages = {}
     actCol = 0
-    nextCol = False
+    #nextCol = False
     probVal = False
+    columns = ['Gender','Age','BP_percentile', 'BPSys_5Hp', 'BPSys_10Hp', 'BPSys_25Hp', 'BPSys_50Hp', 'BPSys_75Hp', 'BPSys_90Hp', 'BPSys_95Hp',
+               'BPDia_5Hp', 'BPDia_10Hp', 'BPDia_25Hp', 'BPDia_50Hp', 'BPDia_75Hp', 'BPDia_90Hp', 'BPDia_95Hp'
+               ]
+    BPdb = pd.DataFrame()
     with open('out.xml', 'r', encoding='utf-8') as XMLinput:
         xml = BeautifulSoup(XMLinput, 'html.parser')
         pages = xml.find_all('page')
@@ -52,7 +56,7 @@ if __name__ == '__main__':
                         elif re.match(r'\D', text.string):
                             #if it is an empty string and our val is probably a value let store it
                             if probVal == True:
-                                pos = eval(textline['bbox'])
+                                pos = eval(text['bbox'])
                                 colsOnPages = updateColumns(colsOnPages, int(page['id']), math.floor(pos[2]), val)
                                 val=''
                                 probVal = False
@@ -60,6 +64,45 @@ if __name__ == '__main__':
                             else:
                                 val = ''
                             break
-    for key in colsOnPages.keys():
-        for k in colsOnPages[key].keys():
-            print('Strona {0}, pozycja {1}, rowek: {2}'.format(key, k, colsOnPages[key][k]))
+    percentiles = [50, 90, 95, 99]
+    for key in colsOnPages.keys():            
+        positions = list(colsOnPages[key].keys())
+        positions.sort()
+        print('Strona {0}, kolumny: {1}'.format(key, positions))
+        Pct = pd.DataFrame()
+        Age = pd.DataFrame()
+        BP = pd.DataFrame()
+        if key % 2 == 0:
+            age = [11]
+        else:
+            age = [1]
+        ageBin = 0
+        for idx, val in enumerate(positions):
+            df = pd.DataFrame({columns[idx+3] : colsOnPages[key][positions[idx]]})
+            BP = pd.concat([BP,df], axis=1)
+        while ageBin < len(BP) / 4:
+            df = pd.DataFrame({'Age' : age * 4})
+            age[0] = age[0] + 1
+            ageBin += 1
+            Age = pd.concat([Age, df], axis=0)
+            df = pd.DataFrame({'BP_percentile':percentiles})
+            Pct = pd.concat([Pct, df], axis=0)
+        Age = Age.reset_index(drop=True)
+        Pct = Pct.reset_index(drop=True)
+        if key == 1 or key == 2:
+            gen = ['M']
+            df = pd.DataFrame({'Gender' : gen * len(BP)})
+            BP = pd.concat([BP,df], axis=1)
+        else:
+            gen = ['F']
+            df = pd.DataFrame({'Gender' : gen * len(BP)})
+            BP = pd.concat([BP,df], axis=1)
+        BP = pd.concat([BP,Age], axis = 1)
+        BP = pd.concat([BP,Pct], axis = 1)
+        BPdb = pd.concat([BPdb, BP], axis = 0)
+        BPdb.reset_index(drop=True)
+    #BPdb.to_csv('test.csv')
+            
+        
+            
+            
