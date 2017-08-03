24e7be1144f13270a52276f62c0c8fdf61c0de9c
panrobot <panrobot@users.noreply.github.com>
2017-08-03 14:05:31 +0200

    Added algorithm to find values of BP

--- data_import.py
+++ data_import.py
@@ -8,9 +8,21 @@ from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     
-    #colSys = [196,223,250,278,305,332,360]
+    def updateColumns(columns, page, position, value):
+        try:
+            columns[page][position].append(value)
+        except:
+            if page not in columns.keys():
+                columns[page] = {position : [value]}
+            else:
+                columns[page][position] = [value]
+        return columns
+            
+        
+    colsOnPages = {}
     actCol = 0
     nextCol = False
+    probVal = False
     with open('out.xml', 'r', encoding='utf-8') as XMLinput:
         xml = BeautifulSoup(XMLinput, 'html.parser')
         pages = xml.find_all('page', id=re.compile('[1]'))
@@ -20,21 +32,21 @@ if __name__ == '__main__':
             for textbox in textboxes:
                 textlines = textbox.find_all('textline')
                 for textline in textlines:
-                    pos = eval(textline['bbox'])
-                    for cs in colSys:
-                        if math.floor(pos[2]) == cs:
-                            nextCol = True
-                            texts = textline.find_all('text')
-                            for text in texts:
-                                if re.match(r'\d', text.string):
-                                    val += text.string
-                                if re.match(r'\n', text.string):
-                                    #print(val)
-                                    val = ''
-                if nextCol:
-                    #print(textline['bbox'])
-                    #print('To była kolumna {0}'.format(actCol))
-                    actCol += 1
-                    #print('Nastepna kolumna {0}'.format(actCol))
-                    nextCol = False
-                    
+                    texts = textline.find_all('text')
+                    for text in texts:
+                        if re.match(r'\d', text.string):
+                            val += text.string
+                            if len(val) > 1:
+                                probVal = True
+                            else:
+                                probVal = False
+                        elif text.string == 't' and val != '':
+                            val = ''
+                            probVal = False
+                        elif re.match(r'\D', text.string):
+                            if probVal == True:
+                                pos = eval(textline['bbox'])
+                                colsOnPages = updateColumns(colsOnPages, page['id'], math.floor(pos[2]), val)
+                                val=''
+                                probVal = False
+                            break
